8524d916f0e50aad837ce6959dc398f2d7c19ad9
Shpakava <darya.shpakava@included.ai>
2025-11-06 22:55:09 +0300

    Fixed header links

--- src/app/components/Header/HeaderPage.tsx
+++ src/app/components/Header/HeaderPage.tsx
@@ -3,18 +3,27 @@
 import { Divider, Input } from 'antd';
 
 import Link from 'next/link';
+import { useParams, usePathname, useSearchParams } from 'next/navigation';
+
+import { HEADER_ITEMS } from '@/app/shared/constants';
 
 import styles from './header.module.css';
 
 export function HeaderPage() {
   const { Search } = Input;
+  const pathname = usePathname();
+  const activeLink = (path: string) => (pathname === path ? 'var(--magenta-5)' : '');
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
-        <Link href={'/'}>Home</Link>
-        <Link href={'/movies'}>Movies</Link>
+        {HEADER_ITEMS.map((item) => (
+          <Link key={item.name} href={item.href} style={{color: activeLink(item.href)}}>
+            {item.name}
+          </Link>
+        ))}
         <span>
-          <Search placeholder="input search text" onSearch={() => {}} style={{ width: 200 }} />
+          <Search placeholder="type to search" onSearch={() => {}} style={{ width: 200 }} />
         </span>
       </div>
       <Divider style={{ background: 'var(--gray-7)', marginBottom: 0 }} />
--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,1 +1,6 @@
 export const MOVIES_API = 'https://api.tvmaze.com/';
+
+export const HEADER_ITEMS = [
+  { name: 'Home', href: '/' },
+  { name: 'Movies', href: '/movies' },
+];
